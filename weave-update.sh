81e96b28f231c73e6c420612bab0004de6f41592
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-04-05 06:47:49 +0200

    weave-update updates weave-core, weave.sh and available-services

--- weave-update.sh
+++ weave-update.sh
@@ -5,22 +5,33 @@ set -e
 
 # This script is used to update weave-core.
 
-echo -e "\e[32mUpdating weave-core and weave.sh...\e[0m"
+update_git_submodules() {
+  git submodule update --init --recursive
+  git submodule foreach '
+    echo "\e[33mUpdating $name...\e[0m"
+    git checkout main || { echo -e "\e[31mFailed to checkout main branch in $name\e[0m"; exit 1; }
+    git pull origin main || { echo -e "\e[31mFailed to pull from main in $name\e[0m"; exit 1; }
+    echo "\e[32m$name updated successfully!\e[0m"]
+  '
+}
 
-# Ensure submodules are initialized and updated
-git submodule update --init --recursive
+echo -e "\e[32mUpdating weave...\e[0m"
 
-# Iterate through each submodule
-git submodule foreach '
-  echo "Pulling latest changes for submodule: $name"
-  git checkout main || { echo "Failed to checkout main branch in $name"; exit 1; }
-  git pull origin main || { echo "Failed to pull from main in $name"; exit 1; }
-'
+# Update weave-core
+update_git_submodules
 
+# Update weave.sh from distant core file
 if [ -f "./weave-core/weave.sh.dist" ]; then
-    echo -e "\e[32mCopying updated weave.sh...\e[0m"
+    echo -e "\e[33mUpdating weave.sh...\e[0m"
+
     cp ./weave-core/weave.sh.dist ./weave.sh
     chmod 755 ./weave.sh
+
+    echo -e "\e[32mweave.sh updated successfully!\e[0m"
 fi
 
-echo "weave-core and weave.sh updated successfully!"
+# Update available services
+cd weave-core
+update_git_submodules
+
+echo -e "\e[32mweave updated successfully!\e[0m"
